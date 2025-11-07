64e96f704acac7694d2692b2d40749fe74cf7d7d
Manivardhan-10k <manivardhan.10kcoders@gmail.com>
2025-11-07 18:24:01 +0530

    latest

--- rev_pro/rev_pro/settings.py
+++ rev_pro/rev_pro/settings.py
@@ -11,13 +11,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 import environ
-env=environ.Env()
-environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env=environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fxez(h*bzdurn7j59)x=m--u$nvp(+!p9b2h%8^pxjz142=kzz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
