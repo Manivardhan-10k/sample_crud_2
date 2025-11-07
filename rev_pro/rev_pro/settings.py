97fe3170f4f2173790921f2c7aeb0333d02d0768
Manivardhan-10k <manivardhan.10kcoders@gmail.com>
2025-11-07 13:32:58 +0530

    allowed render

--- rev_pro/rev_pro/settings.py
+++ rev_pro/rev_pro/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-fxez(h*bzdurn7j59)x=m--u$nvp(+!p9b2h%8^pxjz142=kzz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
